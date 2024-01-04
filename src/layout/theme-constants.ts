a30b7ab5b42b98052ea2cc91bd68fde94da10512
Pranav Gaikwad <pgaikwad@redhat.com>
2024-01-04 09:55:52 -0500

    :sparkles: replace assets
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- src/layout/theme-constants.ts
+++ src/layout/theme-constants.ts
@@ -1,8 +1,8 @@
-import konveyorLogo from "@app/images/konveyor-logo.png";
-import konveyorNavbarLogo from "@app/images/konveyor-navbar-logo.svg";
+import logo from "@app/images/logo.svg";
+import navbarLogo from "@app/images/navbar-logo.svg";
 
-type ThemeType = "konveyor";
-const defaultTheme: ThemeType = "konveyor";
+type ThemeType = "default";
+const defaultTheme: ThemeType = "default";
 
 type ThemeListType = {
   [key in ThemeType]: {
@@ -12,26 +12,19 @@ type ThemeListType = {
     faviconSrc?: string;
     websiteURL: string;
     documentationURL: string;
-    sourceURL: string;
-    discussionForumURL: string;
-    mailingListURL: string;
-    issueTrackingURL: string;
   };
 };
 
 const themeList: ThemeListType = {
-  "konveyor": {
+  "default": {
     name: "Konveyor",
-    logoSrc: konveyorLogo,
-    logoNavbarSrc: konveyorNavbarLogo,
+    logoSrc: logo,
+    logoNavbarSrc: navbarLogo,
     websiteURL: "https://konveyor.io/",
     documentationURL: "https://konveyor.io/community",
-    sourceURL: "https://github.com/konveyor/static-report",
-    discussionForumURL: "https://kubernetes.slack.com/archives/CR85S82A2",
-    mailingListURL: "",
-    issueTrackingURL: "https://github.com/konveyor/community",
   }
 };
+
 
 export const Theme =
   themeList[((window as any)["THEME"] as ThemeType) || defaultTheme];
