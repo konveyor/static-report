5570f6b7dd6accdf8da27292a0fc6089624bd4f4
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2026-06-02 17:59:51 +0200

    Fix static-report
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'remove-module-and-crossorigin',
+      transformIndexHtml(html) {
+        // Remove type="module" and crossorigin to make it work with file:// protocol
+        // Also add defer to scripts to ensure they load in order
+        return html
+          .replace(/type="module"\s+/g, '')
+          .replace(/\s+crossorigin/g, '')
+          .replace(/<script src="/g, '<script defer src="');
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, './src'),
@@ -19,4 +32,15 @@ export default defineConfig({
     },
   },
   base: './',
+  build: {
+    // Generate legacy bundle for file:// protocol compatibility
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        // Use IIFE format instead of ES modules to avoid CORS issues with file://
+        format: 'iife',
+        inlineDynamicImports: true,
+      },
+    },
+  },
 });
